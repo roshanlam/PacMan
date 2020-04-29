f54379e2ecedd415d0f9c7a88a9ef10bd1290320
Roshan Lamichhane <lamichhaner40@gmail.com>
2020-04-28 19:41:07 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>
+            PACMAN - Made for Fun By Roshan Lamichhane
+        </title>
+    </head>
+    
+    <body>
+        <canvas id="myCanvas" width="510" height="510">
+        </canvas>
+        <script type="text/javascript" src="pacman.js"></script>
+        <script type="text/javascript" src="ghost.js"></script>
+        <script type="text/javascript" src="grid.js"></script>
+        <script type="text/javascript" src="game.js"></script>
+    </body>
+
+</html>
